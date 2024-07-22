58707313aa33da5cbd8ecd28e658e2e439f4e4de
juanjose200418 <juanjosejaen11@gmail.com>
2024-07-22 12:24:58 +0200

    add E_1

---
+++ Ejercicios con Objetos/E_1.js
@@ -0,0 +1,5 @@
+/* Escribe una función que tome una cadena de texto como entrada y devuelva la misma cadena en mayúsculas.*/
+function convertirMayusculas(str){
+    return str.toUpperCase()
+}
+console.log(convertirMayusculas("Hoy comí lentejas"))
